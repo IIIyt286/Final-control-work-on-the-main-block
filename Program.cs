862e302c9983a7123cb32543fbd7cdc6197bc1cc
Andrey <temer28658@yandex.ru>
2024-01-20 18:16:04 +0300

    Writing the program code

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Введите размер массива ");
+int len = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[len];
+
+fillArray(array);
+printArray(array);
+string[] finalArr = newArray(array);
+Console.WriteLine("\nНовый массив");
+printArray(finalArr);
+
+
+void fillArray(string[] arr)
+{
+    Console.WriteLine("Введите элементы массива ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+}
+
+void printArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"|{arr[i]}|");
+    }
+}
+
+string[] newArray(string[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+    
+    int index = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            resultArray[index] = arr[i];
+            index++;
+        }
+    }
+
+    return resultArray;
+}
+
+
+
+
+
